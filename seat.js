f655e7fe955ec38528c3876f6729a282a0e10662
Suvam Ghosh <suvam25@kgpian.iitkgp.ac.in>
2026-01-28 23:08:02 +0530

    feat: Introduce seat legend, enhance seat identification with row labels, and implement a full-screen submit confirmation effect with styling updates.

--- seat.js
+++ seat.js
@@ -4,11 +4,13 @@ function genrategrid(rows,columns){
     for(let i=0;i<rows;i++){
         const row=document.createElement('div')
         row.classList.add('row')
+        row.innerHTML=`<div class="seat" style=" padding:20px; margin:0px; background-color:transparent; justify-content: center; align-items: center;">${String.fromCharCode('A'.charCodeAt(0) + i)}</div>`
         for(let j=0;j<columns;j++){
             const seat=document.createElement('div')
             seat.classList.add('seat')
             seat.addEventListener('click',seatclick)
-            seat.textContent=j+1
+            seat.dataset.productID=` ${String.fromCharCode('A'.charCodeAt(0) + i)}${j+1}`;
+            seat.textContent=seat.dataset.productID;
             row.appendChild(seat)
         }
         document.querySelector('.seats').appendChild(row)
@@ -31,8 +33,23 @@ function seatclick(){
 function updateSelectedCount(){
     document.querySelector('.selectedSeats').innerHTML='';
     selectedSeats.forEach(seat=>{
-            document.querySelector('.selectedSeats').innerHTML+=seat.textContent+' ';
+            document.querySelector('.selectedSeats').innerHTML+=seat.dataset.productID+' ';
+            document.querySelector('.total').innerHTML=selectedSeats.length*100;
     })
-
-
 }
+    document.querySelector(".submit").addEventListener("click",()=>{
+        const submit=document.querySelector(".submit");
+       submit.style.backgroundColor="green";
+       submit.style.color="white";
+       submit.style.border="none";
+       submit.style.borderRadius="5px";
+       submit.style.cursor="pointer";
+       submit.style.zIndex="100";
+       submit.style.position="fixed";
+       submit.style.top="50%";
+       submit.style.left="50%";
+       submit.style.transform="translate(-50%,-50%)";
+       submit.style.width="100%";
+       submit.style.height="100%";
+       submit.style.overflow="hidden";
+    });
