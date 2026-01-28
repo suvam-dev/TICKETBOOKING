8c573ecc378be2cbfb252ff3ed5a72f0cab8fb65
Suvam Ghosh <suvam25@kgpian.iitkgp.ac.in>
2026-01-28 14:48:20 +0530

    baic layout done

--- seat.js
+++ seat.js
@@ -1,3 +1,5 @@
+const setsReserver=[];
+const selectedSeats=[];
 function genrategrid(rows,columns){
     for(let i=0;i<rows;i++){
         const row=document.createElement('div')
@@ -5,6 +7,7 @@ function genrategrid(rows,columns){
         for(let j=0;j<columns;j++){
             const seat=document.createElement('div')
             seat.classList.add('seat')
+            seat.addEventListener('click',seatclick)
             seat.textContent=j+1
             row.appendChild(seat)
         }
@@ -12,3 +15,24 @@ function genrategrid(rows,columns){
     }
 }
 genrategrid(10,10);
+function seatclick(){
+    const seat=this;
+    if(seat.classList.contains('selected')){
+        seat.classList.remove('selected');
+        this.style.backgroundColor='yellow';
+        selectedSeats.splice(selectedSeats.indexOf(seat),1);
+    }else{
+        seat.classList.add('selected');
+        this.style.backgroundColor='green';
+        selectedSeats.push(seat);
+    }  
+    updateSelectedCount(); 
+}
+function updateSelectedCount(){
+    document.querySelector('.selectedSeats').innerHTML='';
+    selectedSeats.forEach(seat=>{
+            document.querySelector('.selectedSeats').innerHTML+=seat.textContent+' ';
+    })
+
+
+}
