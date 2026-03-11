b16adc557c3cc9de9740c66ee9bb317c0f835cf6
Suvam Ghosh <suvam25@kgpian.iitkgp.ac.in>
2026-03-11 19:38:51 +0530

    made everythibg connect to each other and all things are functional and the ui and colour schemed changed

--- components/navbar.js
+++ components/navbar.js
@@ -1,15 +1,38 @@
 export default function navbar() {
     const container = document.querySelector('#navbar');
     container.classList.add('navbar');
+    const urlString = window.location.href;
+    const url = new URL(urlString);
+    const lastPart = url.pathname.split('/').pop();
+    console.log("Current page:", lastPart);
+    if (lastPart === "index.html") {
+        container.innerHTML = `
+        <a href="index.html" class="nav-brand">MovieTime</a>
+        <button class="hamburger" id="hamburger" aria-label="Toggle menu">
+            <i class="fa fa-bars"></i>
+        </button>
+        <div class="nav-links" id="nav-links">
+            <input type="text" placeholder="Search movies..." class="search-input">
+            <a href="index.html">Home</a>
+            <a href="./pages/contact.html">Contact</a>
+            <a href="./pages/about.html">About</a>
+            <a href="./pages/login.html">Login</a>       
+        </div>`;
+    } 
+    else { 
     container.innerHTML = `
         <a href="index.html" class="nav-brand">MovieTime</a>
         <button class="hamburger" id="hamburger" aria-label="Toggle menu">
             <i class="fa fa-bars"></i>
         </button>
         <div class="nav-links" id="nav-links">
-            <a href="index.html">Home</a>
-            <a href="pages/login.html">Login</a>       
+            <input type="text" placeholder="Search movies..." class="search-input">
+            <a href="../index.html">Home</a>
+            <a href="contact.html">Contact</a>
+            <a href="about.html">About</a>
+            <a href="login.html">Login</a>       
         </div>`;
+    }
 
     const hamburger = document.getElementById('hamburger');
     const navLinks = document.getElementById('nav-links');
@@ -18,3 +41,4 @@ export default function navbar() {
         hamburger.classList.toggle('active');
     });
 }
+navbar();
